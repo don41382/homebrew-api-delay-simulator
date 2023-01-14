2092c173af6d70a8623b154732b2ff8524a4bde4
Felix Eckhardt <felix.e@gmx.de>
2023-01-14 14:55:37 +0100

    new version

--- api-delay-simulator.rb
+++ api-delay-simulator.rb
@@ -1,9 +1,9 @@
 class ApiDelaySimulator < Formula
   desc "Api-Delay-Simulator written in Rust"
   homepage "https://github.com/don41382/api-delay-simulator"
-  url "https://github.com/don41382/api-delay-simulator/releases/download/0.1.2/api-delay-simulator_0.1.2_x86_64-apple-darwin.tar.gz"
-  sha256 "abeb8da19ce644170cc4a3e3c0799dc0957432d3fbf8454aea316adef89ee8ad"
-  version "0.1.2"
+  url "https://github.com/don41382/api-delay-simulator/releases/download/0.1.3/api-delay-simulator_0.1.3_x86_64-apple-darwin.tar.gz"
+  sha256 "e4fc3d96c9d24cea9d73d97e3dad4d7020ecb97c7747c71001e7e6d1e584e947"
+  version "0.1.3"
 
   def install
     bin.install "api-delay-simulator"
